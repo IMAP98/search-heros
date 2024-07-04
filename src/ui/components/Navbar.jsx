315a942c653ffd433708a20c1e73eb629a13b376
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-07-03 19:37:02 -0600

    Navbar component modified.

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true
+        });
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-3">
             
@@ -36,7 +45,10 @@ export const Navbar = () => {
                     <span className='nav-item nav-link text-info'>
                         Ignacio Moises AP
                     </span>
-                    <button className='nav-item nav-link btn'>
+                    <button 
+                        className='nav-item nav-link btn'
+                        onClick={ onLogout }
+                    >
                         Logout
                     </button>
                 </ul>
