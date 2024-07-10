4e291c08cd54ea45d4769ceed4be1ef27d63c582
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-07-09 19:36:35 -0600

    Final development commit

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -10,9 +10,11 @@ export const LoginPage = () => {
 
     const onLogin = () => {
 
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
         login('Ignacio Moisés AP');
 
-        navigate('/', {
+        navigate(lastPath, {
             replace: true
         });
     }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,26 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 import { LoginPage } from "../auth";
 import { HerosRoutes } from "../heroes";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
+
     return (
         <>
             <Routes>
-                <Route path="login" element={ <LoginPage /> } />
-                <Route path="/*" element={ <HerosRoutes/> } />
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                } 
+                />
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <HerosRoutes />
+                    </PrivateRoute>
+                } />
             </Routes>
         </>
     );
